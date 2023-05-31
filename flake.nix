e1452d114daa2df1b94ab8204a7ba61a12c5718b
Elizabeth Paź <me@ehllie.xyz>
2023-05-31 16:00:27 +0700

    Build for all systems

--- flake.nix
+++ flake.nix
@@ -2,7 +2,16 @@
   inputs = {
     flake3.url = "github:ehllie/flake3";
   };
-  outputs = { flake3, ... }: {
-    packages.aarch64-darwin.default = flake3.packages.aarch64-darwin.hello2;
-  };
+  outputs = { flake3, ... }:
+    let
+      eachSystem = pkgBuild:
+        builtins.listToAttrs (builtins.map
+          (system: { name = system; value = pkgBuild system; })
+          [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]);
+    in
+    {
+      packages = eachSystem (system: {
+        default = flake3.packages.${system}.hello2;
+      });
+    };
 }
